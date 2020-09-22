4b292bf1c232b39a8a51c83ba1dd5b4a7814587d
Rom <rom@Roms-MacBook-Pro.local>
2020-09-22 23:32:19 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+
+var app = new Vue({
+	el: '#app',
+	data: {
+		robots: [],
+		foo: 0,
+		bar: 0,
+		foobar: 0,
+		money: 0,
+		isVictory: false,
+	}
+});
+
+function init() {
+	// add 2 robots
+	new Robot();
+	new Robot();
+
+	for (var i = app.robots.length - 1; i >= 0; i--) {
+		app.robots[i].next();
+	}
+
+}
+
+function game() {
+	
+}
+
+function addFoo() {
+	app.foo++;
+}
+
+function addBar() {
+	app.bar++;
+}
+
+var randomizeName = () => Math.random().toString(36).substring(7);
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+init();
+game();
---
+++ robot.js
@@ -0,0 +1,154 @@
+class Robot {
+	constructor() {
+		this.name = randomizeName();
+		this.available = true;
+		this.activity = 'moving';
+		this.progress = 0;
+		this.maxProgress = 0;
+		this.updateProgress = setInterval(() => {});
+		window.app.robots.push(this);
+		this.next();
+	}
+
+	setName(n) { this.name = n; }
+
+	getName() { return this.name; }
+
+	setAvailable(a) { this.available = a; this.setActivity('none'); }
+
+	isAvailable() { return this.available; }
+
+	setActivity(a) { this.activity = a; }
+
+	getActivity() { return this.activity; }
+
+	setProgress(p) { this.progress = p; }
+
+	getProgress() { return this.progress; }
+
+	setMaxProgress(mp) { this.maxProgress = mp; }
+
+	getMaxProgress() { return this.maxProgress; }
+
+	move() {
+		this.setActivity('moving');
+		this.setMaxProgress(5000);
+		setTimeout(() => this.next(), 5000);
+	}
+
+	mineFoo() {
+		this.setActivity('mining foo');
+		this.setMaxProgress(1000);
+		setTimeout(() => {
+			addFoo();
+			this.next();
+		}, 1000);
+	};
+
+	mineBar() {
+		this.setActivity('mining bar');
+		var randomDuration = getRndInteger(500, 2000);
+		this.setMaxProgress(randomDuration);
+		setTimeout(() => {
+			addBar();
+			this.next();
+		}, randomDuration);
+	}
+
+	buildFoobar() {
+		this.setActivity('building foobar');
+		// take out the bar and the foo so other robots do not use them too;
+		app.foo--;
+		app.bar--;
+		this.setMaxProgress(2000);
+		setTimeout(() => {
+			// get an RNG %;
+			var chance = getRndInteger(0, 100);
+
+			if(chance > 40) {
+				// if worked, add the foobar
+				app.foobar++;
+			} else {
+				// else put the bar back in the inventory
+				app.bar++;
+			}
+			this.next();
+		}, 2000);
+	}
+
+	sellFoobar() {
+		this.setActivity('selling foobar');
+		this.setMaxProgress(10000);
+
+		if(app.foobar > 5) {
+			app.money += 5;
+			app.foobar -= 5;
+		} else {
+			app.money += app.foobar;
+			app.foobar = 0;
+		}
+
+		setTimeout(() => this.next(), 10000);
+	}
+
+	buyRobot() {
+		this.setActivity('buying robot');
+		this.setMaxProgress(0);
+
+		app.foo -= 6;
+		app.money -= 3;
+		var robot = new Robot();
+
+		if(app.robots.length >= 20) {
+			app.isVictory = true;
+		}
+
+		this.next();
+	}
+
+	next() {
+		this.setProgress(0);
+		clearInterval(this.updateProgress);
+		this.updateProgress = setInterval(() => { this.setProgress(this.getProgress() + 1); }, .1);
+
+		if(app.foobar > 0) {
+			if (this.getActivity() === 'selling foobar' || this.getActivity() === 'moving') {
+				return this.sellFoobar();
+			}
+
+			return this.move();
+		}
+
+		if(app.foo >= 6 && app.money >= 3) {
+			if (this.getActivity() === 'buying robot' || this.getActivity() === 'moving') {
+				return this.buyRobot();
+			}
+
+			return this.move();
+		}
+
+		if(app.foo <= 6) {
+			if (this.getActivity() === 'mining foo' || this.getActivity() === 'moving') {
+				return this.mineFoo();
+			}
+
+			return this.move();
+		}
+
+		if(app.bar <= 0) {
+			if (this.getActivity() === 'mining bar' || this.getActivity() === 'moving') {
+				return this.mineBar();
+			}
+
+			return this.move();
+		}
+
+		if(app.foo > 0 && app.bar > 0) {
+			if (this.getActivity() === 'building foobar' || this.getActivity() === 'moving') {
+				return this.buildFoobar();
+			}
+
+			return this.move();
+		}
+	}
+}
